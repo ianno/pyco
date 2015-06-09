31a003b6ab0828093bdad65bdb2f0da4eb4e5183
Antonio Iannopollo <antonio@berkeley.edu>
2015-06-09 02:43:56 -0700

    change logging format with filename

--- pycox/__init__.py
+++ pycox/__init__.py
@@ -7,7 +7,7 @@ ch = logging.StreamHandler()
 #ch.setLevel(logging.DEBUG)
 
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s  - %(lineno)d - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s  - %(filename)s:%(lineno)d - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 
 # add the handlers to the logger

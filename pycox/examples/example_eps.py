b26726a52b288418c89c677271f727be18dfa3d6
Antonio Iannopollo <antonio@berkeley.edu>
2015-06-10 15:34:05 -0700

    add more eps components

--- pycox/examples/example_eps.py
+++ pycox/examples/example_eps.py
@@ -56,3 +56,40 @@ class BusContactorT(ContactorT):
     '''
 
 #Now let's include some components
+class AbstractGenerator(Contract):
+    '''
+    generator OK at beginning. Once broken stays broken.
+    if fails eventually close the contactor
+    '''
+    INPUT_PORTS = [('fail', GeneratorT)]
+    OUTPUT_PORTS = [('c', ACContactorT)]
+    ASSUMPTIONS = '!fail & G(fail -> X fail)'
+    GUARANTEES = 'G(fail -> F ! c)'
+
+class DumbGenerator(Contract):
+    '''
+    Always open
+    '''
+    INPUT_PORTS = [('fail', GeneratorT)]
+    OUTPUT_PORTS = [('c', ACContactorT)]
+    ASSUMPTIONS = '!fail'
+    GUARANTEES = 'G(! c)'
+
+class StdGenerator(Contract):
+    '''
+    closes the contactor if everything is ok
+    '''
+    INPUT_PORTS = [('fail', GeneratorT)]
+    OUTPUT_PORTS = [('c', ACContactorT)]
+    ASSUMPTIONS = '!fail & G(fail -> X fail)'
+    GUARANTEES = 'G(fail -> ! c) & G(!fail -> c)'
+
+class SlowGenerator(Contract):
+    '''
+    closes the contactor if everything is ok.
+    lock after 1 step
+    '''
+    INPUT_PORTS = [('fail', GeneratorT)]
+    OUTPUT_PORTS = [('c', ACContactorT)]
+    ASSUMPTIONS = '!fail & G(fail -> X fail)'
+    GUARANTEES = 'c & G(fail -> X ! c) & G(!fail -> c)'

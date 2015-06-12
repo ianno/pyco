efc0b68a509f6ce4be5bc8b5bc80982863686df3
Antonio Iannopollo <antonio@berkeley.edu>
2015-06-11 21:46:10 -0700

    first test ok

--- pycox/examples/example_eps.py
+++ pycox/examples/example_eps.py
@@ -22,6 +22,7 @@ class BreakableT(BaseType):
     '''
     pass
 
+
 class GeneratorT(BreakableT):
     '''
     generator type
@@ -31,6 +32,12 @@ class GeneratorT(BreakableT):
 class RectifierT(BreakableT):
     '''
     Rectifier
+    '''
+    pass
+
+class ACLoadT(BaseType):
+    '''
+    AC load type
     '''
     pass
 
@@ -54,6 +61,12 @@ class BusContactorT(ContactorT):
     '''
     Bus Contactor
     '''
+
+class DCLoadT(BaseType):
+    '''
+    dc load type
+    '''
+    pass
 
 #Now let's include some components
 class AbstractGenerator(Contract):
@@ -93,3 +106,97 @@ class SlowGenerator(Contract):
     OUTPUT_PORTS = [('c', ACContactorT)]
     ASSUMPTIONS = '!fail & G(fail -> X fail)'
     GUARANTEES = 'c & G(fail -> X ! c) & G(!fail -> c)'
+
+class ACSingleBackup(Contract):
+    '''
+    Backup between ac contactors
+    '''
+    INPUT_PORTS = [('fail1', GeneratorT), ('fail2', GeneratorT)]
+    OUTPUT_PORTS = [('c', ACContactorT)]
+    ASSUMPTIONS = '!fail1 & !fail2 & G(fail1 -> Xfail1) & G(fail2 -> Xfail2)'
+    GUARANTEES = 'G((fail1 | fail2) -> c) & G((!fail1 & !fail2) -> !c)'
+
+class AC2WayBackup(Contract):
+    '''
+    Backup between ac contactors
+    '''
+    INPUT_PORTS = [('fail1', GeneratorT), ('fail2', GeneratorT)]
+    OUTPUT_PORTS = [('c1', ACContactorT), ('c2', ACContactorT)]
+    ASSUMPTIONS = '!fail1 & !fail2 & G(fail1 -> Xfail1) & G(fail2 -> Xfail2)'
+    GUARANTEES = 'G((!fail1 & !fail2) -> (!c1 & !c2)) & \
+                  G((fail1 & !fail2)-> (!c1 & !c2)) & \
+                  G((!fail1 & fail2) -> (c1 & c2)) & \
+                  G((fail1 & fail2) -> (!c1 & c2))'
+
+class AC4WayBackup(Contract):
+    '''
+    Backup between ac contactors
+    '''
+    INPUT_PORTS = [('fail1', GeneratorT), ('fail2', GeneratorT),
+                   ('fail3', GeneratorT), ('fail4', GeneratorT)]
+    OUTPUT_PORTS = [('c1', ACContactorT), ('c2', ACContactorT),
+                    ('c3', ACContactorT), ('c4', ACContactorT)]
+    ASSUMPTIONS = '!fail1 & !fail2 & G(fail1 -> Xfail1) & G(fail2 -> Xfail2) \
+                   G(fail3 -> Xfail3) & G(fail4 -> Xfail4)'
+    GUARANTEES = 'G((!fail1 & !fail4)) -> (!c1 & !c2 & !c3 & !c4)) & \
+                  G((!fail1 & !fail3 & fail4) -> (!c1 & !c2 & c3 & c4)) & \
+                  G((fail1 & !fail2 & !fail4) -> (c1 & c2 & !c3 & !c4)) & \
+                  G((!fail1 & !fail2 & fail3 & fail4) -> (!c1 & c2 & !c3 & c4)) & \
+                  G((fail1 & fail2 & !fail3 & !fail4) -> (c1 & !c2 & c3 & !c4)) & \
+                  G((fail2 & fail3 & (fail1 | fail4)) -> (c1 & !c2 & !c3 & c4))'
+
+class OneSideACLoad(Contract):
+    '''
+    Disconnect rectifier if it breaks
+    '''
+    INPUT_PORTS = [('ext_fail', RectifierT)]
+    OUTPUT_PORTS = [('c', ACContactorT)]
+
+    ASSUMPTIONS = '!ext_fail'
+
+    GUARANTEES = 'G(!ext_fail -> c) & \
+                    G(ext_fail -> !c)'
+
+class DCTwoSideTie(Contract):
+    '''
+    DC tie
+    '''
+    INPUT_PORTS = [('fail1', RectifierT), ('fail2', RectifierT)]
+    OUTPUT_PORTS = [('c1', DCContactorT), ('c2', DCContactorT)]
+    ASSUMPTIONS = '!fail1 & !fail2'
+    GUARANTEES = '''G((!fail1 & !fail2) -> (!c1 & !c2)) &
+                    G((fail1 | fail2) -> (c1 & c2))'''
+
+class DCLoadContactor(Contract):
+    '''
+    Always closed if everything is fine
+    '''
+    INPUT_PORTS = [('fail1', RectifierT), ('fail2', RectifierT)]
+    OUTPUT_PORTS = [('c', DCContactorT)]
+    ASSUMPTIONS = '!fail1 & !fail2 & G(!(fail1 & fail2))'
+    GUARANTEES = 'G(c)'
+
+
+#now add specs
+#case 2gen-2contactors -> 4 ports
+class GenIsolation1(Contract):
+    '''
+    generator 1 is eventually disconnected if faulty.
+    '''
+    INPUT_PORTS = [('fail1', GeneratorT), ('fail2', GeneratorT)]
+    OUTPUT_PORTS = [('c1', ACContactorT), ('c2', ACContactorT)]
+    #ASSUMPTIONS = '''!fail1 & !fail2 & G(!(fail1 & fail2)) &
+    #                 G(fail1 -> Xfail1) & G(fail2 -> Xfail2)'''
+    ASSUMPTIONS = '''!fail1 & G(fail1 -> Xfail1) '''
+    GUARANTEES = 'G(fail1 -> F!c1)'
+
+class GenIsolation2(Contract):
+    '''
+    generator 1 is eventually disconnected if faulty.
+    '''
+    INPUT_PORTS = [('fail1', GeneratorT), ('fail2', GeneratorT)]
+    OUTPUT_PORTS = [('c1', ACContactorT), ('c2', ACContactorT)]
+    #ASSUMPTIONS = '''!fail1 & !fail2 & G(!(fail1 & fail2)) &
+    #                 G(fail1 -> Xfail1) & G(fail2 -> Xfail2)'''
+    ASSUMPTIONS = '''!fail2 & G(fail2 -> Xfail2) '''
+    GUARANTEES = 'G(fail2 -> F!c2)'
--- pycox/tests/test_eps.py
+++ pycox/tests/test_eps.py
@@ -12,3 +12,143 @@ Author: Antonio Iannopollo
 
 import pytest
 from pycox.examples.example_eps import *
+
+@pytest.fixture
+def abstr_gen():
+    '''
+    abstract generator
+    '''
+    comp = AbstractGenerator('AbstractGen')
+    return LibraryComponent('AbstrGenC', comp)
+
+@pytest.fixture
+def dumb_gen():
+    '''
+    dumb generator
+    '''
+    comp = DumbGenerator('DumbGen')
+    return LibraryComponent('DumbGenC', comp)
+
+@pytest.fixture
+def std_gen():
+    '''
+    std generator
+    '''
+    comp = StdGenerator('StdGen')
+    return LibraryComponent('StdGenC', comp)
+
+@pytest.fixture
+def slow_gen():
+    '''
+    slow generator
+    '''
+    comp = SlowGenerator('SlowGen')
+    return LibraryComponent('SlowGenC', comp)
+
+@pytest.fixture
+def ac_single_back():
+    '''
+    ac single backup
+    '''
+    comp = ACSingleBackup('ACSingleBack')
+    return LibraryComponent('ACSingleBackC', comp)
+
+@pytest.fixture
+def ac_2_back():
+    '''
+    ac 2way backup
+    '''
+    comp = AC2WayBackup('AC2Back')
+    return LibraryComponent('AC2BackC', comp)
+
+@pytest.fixture
+def ac_4_back():
+    '''
+    ac 4 way backup
+    '''
+    comp = AC4WayBackup('AC4Back')
+    return LibraryComponent('AC4BackC', comp)
+
+@pytest.fixture
+def ac_load():
+    '''
+    AC load
+    '''
+    comp = OneSideACLoad('ACload')
+    return LibraryComponent('ACLoadC', comp)
+
+@pytest.fixture
+def dc_tie():
+    '''
+    DC tie
+    '''
+    comp = DCTwoSideTie('DC2Tie')
+    return LibraryComponent('DC2TieC', comp)
+
+@pytest.fixture
+def dc_load():
+    '''
+    dc load
+    '''
+    comp = DCLoadContactor('DCLoad')
+    return LibraryComponent('DCLoadC', comp)
+
+@pytest.fixture
+def gen_lib(abstr_gen, dumb_gen, std_gen, slow_gen):
+    '''
+    returns a populated library with only the AC generators
+    '''
+    library = ContractLibrary('gen_lib')
+
+    library.add(abstr_gen)
+    library.add(dumb_gen)
+    library.add(std_gen)
+    library.add(slow_gen)
+
+    #add refinement assertion
+    mapping1 = LibraryPortMapping([abstr_gen, dumb_gen])
+    mapping1.add(abstr_gen.fail, dumb_gen.fail)
+    mapping1.add(abstr_gen.c, dumb_gen.c)
+
+    dumb_gen.add_refinement_assertion(abstr_gen, mapping1)
+    #comp_ab.add_refinement_assertion(comp_meta, mapping)
+
+    mapping2 = LibraryPortMapping([abstr_gen, std_gen])
+    mapping2.add(abstr_gen.fail, std_gen.fail)
+    mapping2.add(abstr_gen.c, std_gen.c)
+
+    std_gen.add_refinement_assertion(abstr_gen, mapping2)
+
+    mapping3 = LibraryPortMapping([abstr_gen, slow_gen])
+    mapping3.add(abstr_gen.fail, slow_gen.fail)
+    mapping3.add(abstr_gen.c, slow_gen.c)
+
+    slow_gen.add_refinement_assertion(abstr_gen, mapping3)
+
+
+    library.verify_library()
+
+    #add type compatibilities
+    library.add_type(GeneratorT)
+    library.add_type(ACContactorT)
+
+    library.add_type_compatibility(GeneratorT, ACContactorT)
+
+    return library
+
+def test_synth_2gen_2c(gen_lib):
+    '''
+    Performs simple synthesis
+    '''
+
+    spec1 = GenIsolation1('G1')
+    spec2 = GenIsolation2('G2')
+
+    interface = SynthesisInterface([spec1, spec2], gen_lib)
+
+    interface.same_block_constraint([spec1.fail1, spec1.c1])
+    interface.same_block_constraint([spec1.fail2, spec1.c2])
+    interface.distinct_ports_constraints([spec1.fail1, spec1.fail2])
+
+    interface.synthesize()
+

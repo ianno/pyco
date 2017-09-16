a4fd3b38be081c0631d5b162f5296aeec1f34368
Antonio Iannopollo <antonio@berkeley.edu>
2017-09-15 23:25:33 -0700

    script

--- run_experiments_date.sh
+++ run_experiments_date.sh
@@ -6,9 +6,9 @@ set -e
 #visualize with
 # tail -n -1 test_synth_6_10_dc_3_* | grep -Eo '[+-]?[0-9]+([.][0-9]+)+'
 
-EPS_TEST_20=true
+EPS_TEST_20=false
 EPS_TEST_40=false
-EPS_TEST_20_PLAIN=false
+EPS_TEST_20_PLAIN=true
 EPS_TEST_40_PLAIN=false
 
 REPS=10
@@ -26,17 +26,24 @@ if $EPS_TEST_20 || $EPS_TEST_40
 then
   for exp in {1..9}
   do
-    rm -f test_synth_6_10_dc_${exp}_*
+    if $EPS_TEST_20
+    then
+      rm -f test_synth_6_10_dc_${exp}_lib20_*
+    fi
+    if $EPS_TEST_40_PLAIN
+    then
+      rm -f test_synth_6_10_dc_${exp}_lib40_*
+    fi
 
     for (( i=1; i<=$REPS; i++ ))
     do
       if $EPS_TEST_20
       then
-        eval "py.test --lib2 --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_lib20_${exp}_${i} 2>&1" $AMP
+        eval "py.test --lib2 --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_${exp}_lib20_${i} 2>&1" $AMP
       fi
       if $EPS_TEST_40
       then
-        eval "py.test --lib4 --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_lib40_${exp}_${i} 2>&1" $AMP
+        eval "py.test --lib4 --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_${exp}_lib40_${i} 2>&1" $AMP
       fi
     done
   done
@@ -46,17 +53,24 @@ if $EPS_TEST_20_PLAIN || $EPS_TEST_40_PLAIN
 then
   for exp in {1..9}
   do
-    rm -f test_synth_6_10_dc_${exp}_*
+    if $EPS_TEST_20_PLAIN
+    then
+      rm -f test_synth_6_10_dc_${exp}_plain_lib20_*
+    fi
+    if $EPS_TEST_40_PLAIN
+    then
+      rm -f test_synth_6_10_dc_${exp}_plain_lib40_*
+    fi
 
     for (( i=1; i<=$REPS; i++ ))
     do
-      if $EPS_TEST_20
+      if $EPS_TEST_20_PLAIN
       then
-        eval "py.test --lib2 --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_plain_lib20_${exp}_${i} 2>&1" $AMP
+        eval "py.test --lib2 --plain --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_${exp}_plain_lib20_${i} 2>&1" $AMP
       fi
-      if $EPS_TEST_40
+      if $EPS_TEST_40_PLAIN
       then
-        eval "py.test --lib4 --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_plain_lib40_${exp}_${i} 2>&1" $AMP
+        eval "py.test --lib4 --plain --timeout=1000 -s pyco/tests/test_eps_facs.py::test_synth_6_10_dc_${exp}spec >> test_synth_6_10_dc_${exp}_plain_lib40_${i} 2>&1" $AMP
       fi
     done
   done
